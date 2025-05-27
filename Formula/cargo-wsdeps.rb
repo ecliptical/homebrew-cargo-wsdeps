aa79715a132d436aa707e33616ed2b6237d3edcd
Peter Nehrer <pnehrer@eclipticalsoftware.com>
2025-05-26 23:51:26 -0400

    Update formula to use binary downloads

--- Formula/cargo-wsdeps.rb
+++ Formula/cargo-wsdeps.rb
@@ -1,14 +1,29 @@
 class CargoWsdeps < Formula
-  desc "A command-line tool for managing workspace dependencies in Rust"
+  desc "Cargo subcommand for managing workspace dependencies in Rust"
   homepage "https://github.com/ecliptical/cargo-wsdeps"
-  url "https://github.com/ecliptical/cargo-wsdeps/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "INSERT_SHA256_HASH_HERE"
+  version "0.1.0"
   license "MIT"
 
-  depends_on "rust" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/ecliptical/cargo-wsdeps/releases/download/v#{version}/cargo-wsdeps-aarch64-apple-darwin.tar.gz"
+      sha256 "1b64fea93ee2654179b3f3c9408ef09190ea583471cfd1c782ef2f17e569467f"
+    end
+    on_intel do
+      url "https://github.com/ecliptical/cargo-wsdeps/releases/download/v#{version}/cargo-wsdeps-x86_64-apple-darwin.tar.gz"
+      sha256 "d87f81667468f223e34ed110dc3662e65fb2de2745e182008d137317193c2ca7"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/ecliptical/cargo-wsdeps/releases/download/v#{version}/cargo-wsdeps-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "b9f33638e1898148b39ec752b8a2146b00bbbd6a9172e28d6276d9ce3cd6a6ed"
+    end
+  end
 
   def install
-    system "cargo", "install", "--locked", "--root", prefix, "cargo-wsdeps"
+    bin.install "cargo-wsdeps"
   end
 
   test do
