f0af06e4dc6a8c62a5c0c1170b3cd7af19a79bd4
pnehrer <312028+pnehrer@users.noreply.github.com>
2026-04-22 22:34:24 +0000

    Update formula to v0.1.4

--- Formula/cargo-wsdeps.rb
+++ Formula/cargo-wsdeps.rb
@@ -1,24 +1,24 @@
 class CargoWsdeps < Formula
   desc "Cargo subcommand for managing workspace dependencies in Rust"
   homepage "https://github.com/ecliptical/cargo-wsdeps"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ecliptical/cargo-wsdeps/releases/download/v#{version}/cargo-wsdeps-aarch64-apple-darwin.tar.gz"
-      sha256 "aaafee79499afe53c76fe1270190534473711670aca55d719d090e1d3a7b2ef8"
+      sha256 "7a9fc9eacc30a4989e7cecdc91599f656072af7365ce0b44842b069f18b26a17"
     end
     on_intel do
       url "https://github.com/ecliptical/cargo-wsdeps/releases/download/v#{version}/cargo-wsdeps-x86_64-apple-darwin.tar.gz"
-      sha256 "92e493e831ce9d005b98b003b4ff18f74f6a15434d6a224e602431223929f677"
+      sha256 "6efdd76d8bafb6bbb918dfbee5dd317ed54c0e5c6264cbf3923b644186d4d90e"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/ecliptical/cargo-wsdeps/releases/download/v#{version}/cargo-wsdeps-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "985853b0cf74150de7e18a41b3505237b131794106a5dea43773fbe97f2b6e5a"
+      sha256 "aaeddbd9eaef89d63f8757e123b652cc4276076264f752297c3d996b9d730ce5"
     end
   end
 
